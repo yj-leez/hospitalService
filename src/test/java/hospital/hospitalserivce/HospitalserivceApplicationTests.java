3c7eec7c2b3cd813ce5483abe0796a68a281c2f8
yoonjoo <yj_leez@naver.com>
2023-05-22 10:18:22 +0900

    first commit

---
+++ src/test/java/hospital/hospitalserivce/HospitalserivceApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.hospitalserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalserivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
